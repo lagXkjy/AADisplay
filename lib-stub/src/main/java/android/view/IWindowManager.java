0e990c2a81642ddce460f48c8014aa47e00831c8
qiang <jqiang.hi@163.com>
2026-08-13 22:48:09 +0800

    代码精简

--- lib-stub/src/main/java/android/view/IWindowManager.java
+++ lib-stub/src/main/java/android/view/IWindowManager.java
@@ -3,35 +3,14 @@ package android.view;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.IInterface;
-import android.os.RemoteException;
 
 public interface IWindowManager extends IInterface {
 
-    void setForcedDisplayDensityForUser(int displayId, int density, int userId);
-
-    void clearForcedDisplayDensityForUser(int displayId, int userId);
-
-    int getWindowingMode(int displayId);
-
-    void setWindowingMode(int displayId, int mode);
-
-    boolean shouldShowWithInsecureKeyguard(int displayId);
-
     void setShouldShowWithInsecureKeyguard(int displayId, boolean shouldShow);
-
-    boolean shouldShowSystemDecors(int displayId);
 
     void setShouldShowSystemDecors(int displayId, boolean shouldShow);
 
-    int getDisplayImePolicy(int displayId);
-
     void setDisplayImePolicy(int displayId, int imePolicy);
-
-    int watchRotation(IRotationWatcher watcher, int displayId) throws RemoteException;
-
-    void removeRotationWatcher(IRotationWatcher watcher) throws RemoteException;
-
-    android.graphics.Bitmap snapshotTaskForRecents(int taskId);
 
     abstract class Stub extends Binder implements IWindowManager {
         public static IWindowManager asInterface(IBinder obj) {
